3afab62b8e402c6663e3c17ffa0f1875fbb8aab6
Anna Pecheanu <Anna.Pecheanu@softwire.com>
2023-07-03 17:40:40 +0300

    Fetch rovers from backend for select

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import logo from "./logo192.png";
+import React, {useEffect, useState} from 'react';
 import {CounterContainer} from "./CounterContainer";
 import {TextTemplate} from "./TextTemplate";
 import {Route, Routes} from "react-router-dom";
 import "./App.scss";
 import mock_rover from "./assets/mock-rover.jpg"
+import Select from 'react-select';
 
 export interface ContainerProps {
     title: string,
@@ -13,8 +13,32 @@ export interface ContainerProps {
     image_src : string
 }
 
+interface Rover {
+    value: string,
+    label: string
+}
+
+/*
+const hardcodedRovers: Rover[] = [
+    { value: 'Curiosity', label: 'Curiosity'},
+    { value: 'Opportunity', label: 'Opportunity'},
+    { value: 'Perseverance', label: 'Perseverance'},
+    { value: 'Spirit', label: 'Spirit'},
+];*/
+
 function App() {
     const loremIpsum : string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+    const [rovers, setRovers] = useState<Rover[]>([]);
+
+    useEffect(() : void => {
+        fetch("http://localhost:8000/rovers/",
+            {mode: 'cors'})
+            .then(response => response.json())
+            .then((rovers) : void => {
+                console.log(rovers);
+                setRovers(rovers.map((rover : any) : Rover => { return { label: rover.name, value: rover.name }}));
+            })}, []);
+
     return (
       <div className="wrapper">
           <nav>
@@ -23,6 +47,7 @@ function App() {
                   <li onClick={() : void => { window.location.href = "/counter"}}> Counter </li>
               </ul>
           </nav>
+          <Select className="select-styling" options={rovers}/>
           <Routes>
               <Route path="/" element = {
                   <div className="container">
