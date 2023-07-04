28aa893a86cbe5bb0e6b5e9426ece4fb0542a2d4
Anna Pecheanu <Anna.Pecheanu@softwire.com>
2023-07-04 10:53:40 +0300

    Added both selects, for camera and for rover

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import {TextTemplate} from "./TextTemplate";
 import {Route, Routes} from "react-router-dom";
 import "./App.scss";
 import mock_rover from "./assets/mock-rover.jpg"
-import Select from 'react-select';
+import Select, {SingleValue} from 'react-select';
 
 export interface ContainerProps {
     title: string,
@@ -14,6 +14,11 @@ export interface ContainerProps {
 }
 
 interface Rover {
+    name: string,
+    cameras: string[]
+}
+
+interface SelectOptions {
     value: string,
     label: string
 }
@@ -29,15 +34,29 @@ const hardcodedRovers: Rover[] = [
 function App() {
     const loremIpsum : string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
     const [rovers, setRovers] = useState<Rover[]>([]);
+    const [currentRover, setCurrentRover] = useState<string>();
+    const [currentCameras, setCurrentCameras] = useState<SelectOptions[]>([]);
+
+    useEffect(() : void => {
+        if (currentRover) {
+            setCurrentCameras(getCamerasForRover(currentRover));
+        } else {
+            setCurrentCameras([]);
+        }
+    }, [currentRover]);
 
     useEffect(() : void => {
         fetch("http://localhost:8000/rovers/",
             {mode: 'cors'})
             .then(response => response.json())
             .then((rovers) : void => {
-                console.log(rovers);
-                setRovers(rovers.map((rover : any) : Rover => { return { label: rover.name, value: rover.name }}));
+                setRovers(rovers);
             })}, []);
+
+    function getCamerasForRover(roverName : string) : SelectOptions[] {
+        return roverName.length >= 1 ? rovers.filter((rover : Rover) : boolean => roverName === rover.name)[0].cameras
+                                                  .map((camera : string) : SelectOptions => {return { label: camera, value: camera}}) : [];
+    }
 
     return (
       <div className="wrapper">
@@ -47,7 +66,21 @@ function App() {
                   <li onClick={() : void => { window.location.href = "/counter"}}> Counter </li>
               </ul>
           </nav>
-          <Select className="select-styling" options={rovers}/>
+          <div className="select-container">
+              <Select
+                  placeholder="Rover"
+                  className="select-styling"
+                  onChange={(choice : SingleValue<SelectOptions>) : void => {
+                      setCurrentRover(choice ? choice.value : "");
+                      console.log(getCamerasForRover(choice ? choice.value : ""));
+                      setCurrentCameras(getCamerasForRover(choice ? choice.value : ""));
+                  }}
+                  options={rovers.map((rover : Rover) : SelectOptions => {
+                    return { label: rover.name, value : rover.name}
+              })}
+                  />
+              <Select placeholder="Camera" className="select-styling" options={currentCameras}/>
+          </div>
           <Routes>
               <Route path="/" element = {
                   <div className="container">
