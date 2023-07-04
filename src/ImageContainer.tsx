275f03bf56da4ed51878972bd5fc7c8a9603f4f7
Anna Pecheanu <Anna.Pecheanu@softwire.com>
2023-07-04 15:26:39 +0300

    Added Lightbox

--- src/ImageContainer.tsx
+++ src/ImageContainer.tsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import "./ImageContainer.scss";
-
+import Lightbox, {Slide, SlideImage} from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
 interface ImageContainerInterface {
     urls: string[];
 }
 export const ImageContainer : React.FC<ImageContainerInterface> = ({urls} : ImageContainerInterface) => {
-    const images = urls.length !== 0 ? urls.map((url : string) => <img key={url} src = {url}/>) : <p> No photos available! </p>;
+    const images = urls.length !== 0 ? urls.map((url : string) => <img onClick={() => setOpen(true)} key={url} src = {url}/>) :
+        <p> No photos available! </p>;
+    const [open, setOpen] = useState<boolean>(false);
+    const slides : SlideImage[] = [];
+    urls.forEach((url : string) : number => slides.push( {src: url} ));
     return (
-        <div className="gallery">{images}</div>
+        <div>
+            <div
+                onClick={() => setOpen(true)}
+                className="gallery">{images}</div>
+            {open && <Lightbox
+                open={open}
+                close={() => setOpen(false)}
+                slides={slides}
+            ></Lightbox>}
+        </div>
     )
 }
