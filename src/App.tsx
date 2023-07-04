b38e31c6350673074feacb5d16092d08286c9483
Anna Pecheanu <Anna.Pecheanu@softwire.com>
2023-07-04 11:08:21 +0300

    Fetch photos for given camera and rover

--- src/App.tsx
+++ src/App.tsx
@@ -23,27 +23,13 @@ interface SelectOptions {
     label: string
 }
 
-/*
-const hardcodedRovers: Rover[] = [
-    { value: 'Curiosity', label: 'Curiosity'},
-    { value: 'Opportunity', label: 'Opportunity'},
-    { value: 'Perseverance', label: 'Perseverance'},
-    { value: 'Spirit', label: 'Spirit'},
-];*/
-
 function App() {
     const loremIpsum : string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
     const [rovers, setRovers] = useState<Rover[]>([]);
     const [currentRover, setCurrentRover] = useState<string>();
-    const [currentCameras, setCurrentCameras] = useState<SelectOptions[]>([]);
-
-    useEffect(() : void => {
-        if (currentRover) {
-            setCurrentCameras(getCamerasForRover(currentRover));
-        } else {
-            setCurrentCameras([]);
-        }
-    }, [currentRover]);
+    const [currentCameras, setCurrentCameras] = useState<SelectOptions[]>([])
+    const [currentCamera, setCurrentCamera] = useState<string>();
+    const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
 
     useEffect(() : void => {
         fetch("http://localhost:8000/rovers/",
@@ -53,9 +39,21 @@ function App() {
                 setRovers(rovers);
             })}, []);
 
+    useEffect(() : void => {
+        fetch(`http://localhost:8000/rovers/${currentRover}/photos/${currentCamera}/1000`)
+            .then(response => response.json())
+            .then((parsedResponse) : void => {
+                console.log(parsedResponse);
+            });
+    }, [isSubmitting]);
+
     function getCamerasForRover(roverName : string) : SelectOptions[] {
         return roverName.length >= 1 ? rovers.filter((rover : Rover) : boolean => roverName === rover.name)[0].cameras
                                                   .map((camera : string) : SelectOptions => {return { label: camera, value: camera}}) : [];
+    }
+
+    function handleSubmit() : void {
+        setIsSubmitting(!isSubmitting);
     }
 
     return (
@@ -63,7 +61,7 @@ function App() {
           <nav>
               <ul>
                   <li onClick={(): void => { window.location.href = "/" }}> Home </li>
-                  <li onClick={() : void => { window.location.href = "/counter"}}> Counter </li>
+                  <li onClick={() : void => { window.location.href = "/counter" }}> Counter </li>
               </ul>
           </nav>
           <div className="select-container">
@@ -72,14 +70,19 @@ function App() {
                   className="select-styling"
                   onChange={(choice : SingleValue<SelectOptions>) : void => {
                       setCurrentRover(choice ? choice.value : "");
-                      console.log(getCamerasForRover(choice ? choice.value : ""));
                       setCurrentCameras(getCamerasForRover(choice ? choice.value : ""));
                   }}
                   options={rovers.map((rover : Rover) : SelectOptions => {
                     return { label: rover.name, value : rover.name}
               })}
                   />
-              <Select placeholder="Camera" className="select-styling" options={currentCameras}/>
+              <Select
+                  isDisabled={currentCameras.length === 0}
+                  placeholder="Camera"
+                  className="select-styling"
+                  onChange={(choice : SingleValue<SelectOptions>) : void => { setCurrentCamera(choice ? choice.value : "")}}
+                  options={currentCameras}/>
+              <button onClick={handleSubmit}> Search! </button>
           </div>
           <Routes>
               <Route path="/" element = {
